81b2b7410602109760d8cdfbe71f6b1141e3b3c2
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-13 23:38:45 -0300

    up

---
+++ src/portf/indexportf.jsx
@@ -0,0 +1,11 @@
+import styles from "./stylesportf.module.css";
+
+export default function Protf() {
+  return (
+    <>
+      <div className={styles.contener}>
+        <h1>protf</h1>
+      </div>
+    </>
+  );
+}
